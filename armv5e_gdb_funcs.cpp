7dbf8421a450fbbe3efbff377affefa6c9a11338
Bruno Albertini <balbertini@gmail.com>
2006-06-29 17:58:42 +0000

    Fixed header null brief, file and date (doxygen).

--- armv5e_gdb_funcs.cpp
+++ armv5e_gdb_funcs.cpp
@@ -9,7 +9,7 @@
  *            IC-UNICAMP
  *            http://www.lsc.ic.unicamp.br/
  *
- * @version   version?
+ * @version   1.0
  * @date      Mon, 19 Jun 2006 15:33:28 -0300
  * 
  * @brief     The ArchC ARMv5e functional model.
