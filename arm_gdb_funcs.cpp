1cd807524f9d7e5ea063ec49373f33ee84b4d5ac
maxiwell <maxiwell@gmail.com>
2016-08-11 23:52:09 -0300

    [gdb] Fixing data access in gdb functions

--- arm_gdb_funcs.cpp
+++ arm_gdb_funcs.cpp
@@ -46,10 +46,10 @@ void arm::reg_write( int reg, ac_word value ) {
 }
 
 unsigned char arm::mem_read( unsigned int address ) {
-  return IM->read_byte(address);
+  return DATA_PORT->read_byte(address);
 }
 
 void arm::mem_write( unsigned int address, unsigned char byte ) {
-  IM->write_byte( address, byte );
+  DATA_PORT->write_byte( address, byte );
 }
 
